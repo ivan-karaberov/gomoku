8415e39037d1cd39d83b5afc4ef930dbc2170408
ivan-karaberov <karaberovivan123456@gmail.com>
2024-03-07 17:52:00 +0300

    feat: add function for ai move

--- src/game.py
+++ src/game.py
@@ -7,12 +7,12 @@ class Game:
         self.finished = False
         self.restart(player_index)
 
-    def restart(self, player_index=1):
+    def restart(self, player_index=1) -> None:
         self.board = Board(self.size)
         self.ai_color = player_index * -1
         self.finished = False
 
-    def play(self, row: int, col: int):
+    def play(self, row: int, col: int) -> bool:
         position = (row, col)
         
         if self.board.set_value(position, self.now_move):
@@ -23,7 +23,9 @@ class Game:
         return False
 
     def aiplay(self):
-        pass
+        move = self.ai.get_move()
+        self.finished = self.board.is_terminal()
+        return move
 
     def change_move_player(self):
         self.now_move *= -1
